2735a646e44ebe35777d9ab44e89894770e77eb2
Marcus Vinicius <marcosvinicius.bob@gmail.com>
2024-10-15 15:52:10 -0300

    Estrutura básica

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# imobiliaria
+# Projeto 3 - Site Imobiliaria
+
+Projeto de site de uma imobiliaria utilizando HTML, CSS (Flex e Grid), JavaScript, Fetch, JSON, LocalStorage
---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+// Simula um JSON online (Banco de Dados)
+const listaImoveis = [
+{
+    id: 1,
+    titulo: 'Apart. 3 dorm. Papicu',
+    descricao: 'Lindo apartamento de 3 dormitórios localizados num dos melhores bairros de Fortaleza',
+    valor: 'R$ 500.000',
+    area: '90 m²',
+    quartos: 3,
+    tipo: 'apartamento',
+    localizacao: 'Santos Dummont, Papicu',
+    mapa:'link do maps',
+    venda_aluguel: 'venda',
+    finalidade: 'residencial',
+    fotos [
+        'link1',
+        'link2',
+        'link3',
+        'link4',
+        'link5',
+    ],
+}
+];
